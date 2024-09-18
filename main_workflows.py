405d42b4eb76fe1d27e6fd27847ee76edad2bb15
Dmytro Petrushenko <dpetrushenko@eisgroup.com>
2024-09-18 15:57:13 +0300

    changed agent names and other small things

--- main_workflows.py
+++ main_workflows.py
@@ -1,3 +1,23 @@
 from workflows.pentest_team.launcher.worlflows_launcher import start_investigation_team_workflow
 
-start_investigation_team_workflow("Investigate this host: 63.251.228.70.")
+
+
+
+task = f"""
+I have been provided with data from Nessus. Conduct an investigation of the host 63.251.228.70 according to this data only.
+
+Nessus data:
+    Vulnerabilities List:
+    - Nessus UDP Scanner
+    - Apache HTTP Server Version
+    - Common Platform Enumeration (CPE)
+    - HTTP Server Type and Version
+    - HyperText Transfer Protocol (HTTP) Information
+    - Nessus Scan Information
+    - Non-compliant Strict Transport Security (STS)
+    - Strict Transport Security (STS) Detection
+    - Traceroute Information
+    - Web Server No 404 Error Code Check
+"""
+
+start_investigation_team_workflow(task)
